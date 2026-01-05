2c3a69cadb8d4c642a55625dec0c2ee354945cd8
protap55 <protapdutta500@gmail.com>
2026-01-05 21:24:32 +0600

    reverse done

--- array-loop.js
+++ array-loop.js
@@ -1,9 +1,11 @@
-let nam = ["Raja", "Taj", "Yash", "Robin"];
+// let nam = ["Raja", "Taj", "Yash", "Robin"];
 
 // for (let friend of nam) {
 //   console.log(friend);
 // }
 
-for (let i = 0; i < nam.length; i++) {
-  console.log(nam[i]);
-}
+// for (let i = 0; i < nam.length; i++) {
+//   console.log(nam[i]);
+// }
+
+
---
+++ reverse.js
@@ -0,0 +1,6 @@
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let rev = [];
+for (let num of nums) {
+  rev.unshift(num);
+}
+console.log(rev);
