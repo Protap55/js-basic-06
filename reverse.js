0564d9d2b99b84f1898e9f322b238376f8d4bb8d
protap55 <protapdutta500@gmail.com>
2026-01-05 21:45:02 +0600

    more func

--- reverse.js
+++ reverse.js
@@ -1,6 +1,27 @@
-let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let rev = [];
-for (let num of nums) {
-  rev.unshift(num);
-}
-console.log(rev);
+// let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let rev = [];
+// for (let num of nums) {
+//   rev.unshift(num);
+// }
+// console.log(rev);
+
+// let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = nums.length - 1; i >= 0; i--) {
+//   console.log(nums[i]);
+// }
+
+// let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// nums.reverse();
+// console.log(nums);
+
+// let nam = ["Raja", "Taj", "Yash", "Robin"];
+
+// nam.reverse();
+// console.log(nam);
+
+let nam = "Raja";
+let mod = nam.split("").reverse("").join("");
+let rod = nam.split("").reverse("").join();
+console.log(mod);
